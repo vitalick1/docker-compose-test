27a707e1aa1d19c3a7f7c532ec29ba3cae93de69
Vitalie Lisnic <Vitalie.Lisnic@yopeso.com>
2015-07-30 18:25:14 +0300

    integrated gearman job server

--- app1/index.php
+++ app1/index.php
@@ -9,3 +9,15 @@ $redis = new Redis();
 $redis->connect('redis', '6379');
 $redis->set("test", "aaa");
 print_r($redis->get("test"));
+
+
+$gearman = new \GearmanClient();
+$gearman->addServer("gearman");
+$gearman->addTask("test", json_encode(['test'=>'test']));
+
+
+$gearmanWorker = new \GearmanWorker();
+
+$gearmanWorker->addFunction('test', function(\GearmanJob $job){
+    print_r($job->workload());
+});
