8d0becb3ccb786e68da819a5a767efbf94c78e8a
Vitalie Lisnic <Vitalie.Lisnic@yopeso.com>
2015-07-30 17:56:22 +0300

    added support for redis

--- app1/index.php
+++ app1/index.php
@@ -1,6 +1,11 @@
 <?php
-
-
 $pdo = new \PDO('mysql:dbname=app1;host=mysql;port:3307', 'root', 'root');
-
 $result = $pdo->query('SELECT 1;');
+
+print_r($result);
+
+
+$redis = new Redis();
+$redis->connect('redis', '6379');
+$redis->set("test", "aaa");
+print_r($redis->get("test"));
